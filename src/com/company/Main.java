ad82cc55dee4eca999d8e5ed9d7b8ce8a8e66140
Gherghina <mar_gherasim@yahoo.com>
2019-09-18 02:45:50 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+               System.out.println("Hello\nMaria!\n");
+        // Comentariu - As fi vrut sa las un rand liber cu \n dar nu mi-a mers peste tot ....
+
+
+           // Suma a 2 numere ...
+        System.out.println("The sum of two numbers (12 and 30).");
+        int y = 12;
+        float x = 30;
+      float result;
+        result = x + y;
+        System.out.println("The sum is "+result);
+        System.out.println();
+        System.out.println("The division of two numbers (30 and 12).");
+        float answer;
+        answer = x / y;
+        System.out.println("The division is "+answer);
+        System.out.println();
+
+        int a=-5;
+        int b=8;
+        int c=6;
+        int d=b*c;
+        System.out.println(a+d);
+        System.out.println();
+        int e=55;
+        int f=9;
+        int g=e+f;
+        System.out.println(g%f);
+      System.out.println();
+        int h=20;
+        int i=-3;
+        int l=5;
+        int j=i*l;
+        System.out.println(h+j/b);
+      System.out.println();
+      int m=15;
+      int n=3;
+      int o=2;
+      int p=m/n*o;
+      System.out.println(l+p-(b%n));
+        System.out.println();
+
+
+
+
+
+
+
+
+    }
+}
